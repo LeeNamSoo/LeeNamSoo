d5ff3bbf72d23f12a8c076144a67fadb7ff2c96a
LeeNamSoo <lns960712@gmail.com>
2019-01-06 18:13:30 +0900

    add_new_file

---
+++ X보다 작은수.cpp
@@ -0,0 +1,14 @@
+# include<stdio.h>
+# define MAX 10001
+
+int main(void){
+	int n,x,a[MAX];
+	scanf("%d%d",&n,&x);
+	for(int i=0; i<n; i++){
+		scanf("%d",a+i); 
+	}
+	for(int i=0; i<n; i++){
+		if(a[i]<x) printf("%d ",a[i]);
+	}
+	return 0;
+}
